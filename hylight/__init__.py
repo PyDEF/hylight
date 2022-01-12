97ec13142b0be3cf076fdcafdda46ff3e3b1db98
Theo Cavignac <theo.cavignac@ecl16.ec-lyon.fr>
2022-01-12 10:43:07 +0100

    Setup for installation

---
+++ hylight/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.1.0"
+
+from . import constants
+from . import loader
+from . import mode
+from . import mono_phonon
+from . import multi_phonons
--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="hylight",
-    version="0.1.0",
-    author="Théo Cavignac",
-    author_email="theo.cavignac@gmail.com",
-    description="Hylight is post-processing package for luminescence related ab initio computations.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/PyDEF/hylight",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    keywords=[
-        "chemistry",
-        "computational chemistry",
-    ],
-)
+setup()
