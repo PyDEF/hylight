07aeb4498332d45a5f6c383b829b7c7ec6da8df6
Theo Cavignac <theo.cavignac@ecl16.ec-lyon.fr>
2022-02-10 12:38:10 +0000

    Add constants

--- hylight/constants.py
+++ hylight/constants.py
@@ -1,6 +1,6 @@
-h_si = 6.62607015e-34  # SI
-c_si = 2.99792e8  # SI
-h_c = h_si * c_si
+h_si = 6.62607015e-34  # SI (J.s)
+c_si = 2.99792e8  # SI (m.s-1)
+h_c = h_si * c_si  # SI (J.m)
 eV_in_J = 1.602176634e-19
 two_pi = 6.283185307179586
 pi = 3.141592653589793
@@ -10,6 +10,8 @@ hbar = h_si / two_pi / eV_in_J  # ev.s.rad-1
 atomic_mass = 1.67e-27  # SI
 
 THz_in_meV = 4.135665538536
+
+cm1_in_J = 100 * h_c
 
 kb = 1.38064852e-23  # J.K-1 Boltzmann constant
 kb_eV = kb / eV_in_J  # eV.K-1
