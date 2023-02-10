7d2414f5f0cc691b0951b8dec7545d986a62831a
Théo Cavignac <theo.cavignac@gmail.com>
2023-02-10 16:53:19 +0100

    Trailing spaces

--- hylight/mode.py
+++ hylight/mode.py
@@ -1,17 +1,17 @@
 "Vibrational mode and related utilities."
 # License
 #     Copyright (C) 2023  PyDEF development team
-# 
+#
 #     This program is free software: you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License as published by
 #     the Free Software Foundation, either version 3 of the License, or
 #     (at your option) any later version.
-# 
+#
 #     This program is distributed in the hope that it will be useful,
 #     but WITHOUT ANY WARRANTY; without even the implied warranty of
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #     GNU General Public License for more details.
-# 
+#
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from .constants import eV_in_J, atomic_mass, hbar_si, two_pi
